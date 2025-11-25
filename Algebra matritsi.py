813948007a17c11ef663fb6d92e54e91058a9fda
Максим Д. <115506198+MaksimRider@users.noreply.github.com>
2025-11-25 15:54:15 +0200

    Refactor Gaussian elimination and user input handling

--- Algebra matritsi.py
+++ Algebra matritsi.py
@@ -4,17 +4,13 @@ import numpy as np
 def gaussian_elimination(matrix, b):
     n = len(matrix)
 
-    #Створюємо матрицю [A|b]
     augmented_matrix = np.hstack((matrix, np.array(b).reshape(-1, 1)))
 
-    #Прямий хід
     for i in range(n):
-        #Підраховуємо множник для поточного рядка
         for j in range(i + 1, n):
             factor = augmented_matrix[j][i] / augmented_matrix[i][i]
             augmented_matrix[j][i:] -= factor * augmented_matrix[i][i:]
 
-    #Зворотній хід
     x = np.zeros(n)
     for i in range(n - 1, -1, -1):
         x[i] = augmented_matrix[i][-1] / augmented_matrix[i][i]
@@ -34,25 +30,20 @@ def kramer_method(matrix, b):
 
     solutions = []
     for i in range(n):
-        #Створюємо копію матриці A і замінюємо i-тий стовпець на вектор b
         matrix_i = matrix.copy()
         matrix_i[:, i] = b
 
-        #Знаходимо детермінант матриці заміщеної
         det_A_i = np.linalg.det(matrix_i)
 
-        #Знаходимо розв'язок для i-тої невідомої
         xi = det_A_i / det_A
         solutions.append(xi)
 
     return solutions
 
-#Отримати розмірність матриці від користувача
 dimensions = input("Введіть розмірність матриці (у форматі 3-3, якщо матриця 3x3): ").split('-')
 n = int(dimensions[0])
 m = int(dimensions[1])
 
-# Ввід матриці та вектора b
 print("Введіть матрицю A:")
 matrix = []
 for i in range(n):
@@ -62,11 +53,9 @@ for i in range(n):
 print("Введіть вектор b:")
 b = list(map(float, input().split()))
 
-# Перетворення списків у масиви NumPy
 matrix = np.array(matrix)
 b = np.array(b)
 
-# Запит користувача щодо методу розв'язку
 method = input("Виберіть метод (Гауса або Крамера): ")
 
 if method.lower() == "гауса":
